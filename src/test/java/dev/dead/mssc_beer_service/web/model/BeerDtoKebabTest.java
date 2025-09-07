8d0ef5c05ebca56d7a2063f5ba9bb5927e451c0d
deadboyccc <ahmed.mohammed.ali9999@gmail.com>
2025-09-07 22:25:25 +0300

    added simple jackson tests for kebab casing

---
+++ src/test/java/dev/dead/mssc_beer_service/web/model/BeerDtoKebabTest.java
@@ -0,0 +1,41 @@
+package dev.dead.mssc_beer_service.web.model;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.json.JsonTest;
+import org.springframework.test.context.ActiveProfiles;
+
+import java.math.BigDecimal;
+import java.time.OffsetDateTime;
+import java.util.UUID;
+
+@ActiveProfiles("kebab")
+@JsonTest
+public class BeerDtoKebabTest {
+
+    @Autowired
+    ObjectMapper objectMapper;
+    private BeerDto validBeer;
+
+    @BeforeEach
+    void setUp() {
+        validBeer = BeerDto.builder()
+                .id(UUID.randomUUID())
+                .beerName("Test Beer")
+                .createdDate(OffsetDateTime.now())
+                .lastModified(OffsetDateTime.now())
+                .beerStyle(BeerStyleEnum.ALE)
+                .price(BigDecimal.valueOf(2.99))
+                .upc(123456789012L)
+                .build();
+    }
+
+    @Test
+    void testKebab() throws JsonProcessingException {
+        String json = objectMapper.writeValueAsString(validBeer);
+        System.out.println(json);
+    }
+}
